ce31156d1faec01b5bd927e0a6ea796b07e3488f
pafinkbeiner <paul-d-f@web.de>
2022-12-29 15:02:26 +0100

    started with linux python camera

--- Linux/Camera.py
+++ Linux/Camera.py
@@ -1,10 +1,18 @@
+import cv2
 
 class Camera:
 
     def __init__(self) -> None:
-
+        self.webcam = cv2.VideoCapture(0)
         print("Linux Camera Class initialized...")
 
     def getFrame(self):
+        _, frame = self.webcam.read()
+        return frame
 
+    def saveFrameAs(_self, frame, name):
+        raise NotImplementedError("Save Frame As Function not implemented yet")
 
+    def exit(self):
+        self.webcam.release()
+        cv2.destroyAllWindows()
